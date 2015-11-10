d5cd348d09d53d348eb28207b926ecc17546e816
am1k <vitaliy_bog@mail.ru>
2015-11-10 13:25:08 +0200

    add closeParentViews

--- js/views/buttonView.js
+++ js/views/buttonView.js
@@ -12,29 +12,35 @@ define([
         idName: '#close-popup',
 
         template: _.template(myTemplate),
+
         events: {
             'click': 'action'
         },
 
-        initialize: function(time, cb){
-            this.action = function(){
-                this.remove();
-                cb();
-            };
+        initialize: function(options){
             this.model = new TimerModel({
-                time: time
+                time: options.time
             });
             this.listenTo(this.model, 'change:time', function(){
                 this.render();
             });
             this.listenTo(this.model, 'change:finished', this.action);
             this.render();
+            return this;
         },
 
         render: function(){
             this.$el.html(this.template(this.model.toJSON() ));
             return this;
+        },
+        action: function(){
+            try{
+                this.parent.close();
+            }catch(e){
+                new Error('parent view isn\'t exist');
+            }
         }
+
     });
 
     return buttonView;
--- js/views/popupView.js
+++ js/views/popupView.js
@@ -17,32 +17,41 @@ define([
         template: _.template(myTemplate),
 
         events: {
-            'click .close' : 'closePopup',
-            'click .btn-default': 'closePopup'
+            'click .close' : 'close'
         },
 
         initialize: function(){
+            this.childrenViews = [];
             this.render();
         },
 
         render: function(){
             this.$el.html(this.template(this.model.toJSON() ));
-            this.$el.find('.modal-footer').append((new ButtonView(5, function(){
-                this.remove();
-            }.bind(this))).$el);
+            this.appendChildView(new ButtonView({time: 5}), '.modal-footer');
+            //this.$el.find('.modal-footer').append((new ButtonView(5)).$el);
+            //http://stackoverflow.com/questions/31963596/backbonejs-is-passing-a-parents-element-reference-to-a-child-view-a-good-pract
+
             this.$el.find('.modal.fade').addClass('in');
             return this;
-        },
-
-        closePopup: function(){
-            this.remove();
         }
-
-
-
-
-
     });
+
+    // вынос глобально метода для закрытия
+    Backbone.View.prototype.close = function(){
+        this.childrenViews && this.childrenViews.forEach(function(view){
+            view.close();
+        });
+
+        this.remove();
+    };
+
+    // вынос глобально для нахождения дочерних вью
+    Backbone.View.prototype.appendChildView = function(view, element){
+        this.childrenViews.push(view);
+        //проброс дочернего вью в родительный
+        view.parent = this;
+        this.$el.find(element).append((view).$el);
+    };
 
     return popupView;
 
